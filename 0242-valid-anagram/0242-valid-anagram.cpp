cc5c9b2c2f40e620006622665c13140845153c4d
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-10 01:30:32 +0530

    Time: 7 ms (19.01%), Space: 9.5 MB (92.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,9 +4,6 @@ public:
         if(s.size()!=t.size())return false;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        for(int x = 0 ;  x < t.size() ; x++){
-            if(s[x]!=t[x])return false;
-        }
-        return true;
+        return s==t;
     }
 };
