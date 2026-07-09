11f18217bc39958e92c9cae158110e8d63851a00
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-10 00:47:27 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.83%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,15 +4,12 @@ public:
         if(t.size()!=s.size())return false;
         unordered_map<char,char> mp1;
         unordered_map<char,char> mp2;
-        for(int i = 0;i<t.size();i++){
+        for (int i = 0; i < s.size(); i++) {
+            if (mp1.count(s[i]) && mp1[s[i]] != t[i]) return false;
+            if (mp2.count(t[i]) && mp2[t[i]] != s[i]) return false;
+
             mp1[s[i]] = t[i];
-        }
-        for(int i = 0;i<t.size();i++){
             mp2[t[i]] = s[i];
-        }
-        for(int i = 0 ; i < t.size() ; i++){
-            if(mp1[s[i]]!=t[i])return false;
-            if(mp2[t[i]]!=s[i])return false;
         }
         return true;
     }
