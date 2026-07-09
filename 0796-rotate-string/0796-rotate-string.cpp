a33e057c43b89661fa8591ebbb01575b1d2c4349
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-10 01:26:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (51.43%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,12 +3,9 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size())return false;
         s+=s;
-        int i = 0;
-        int j = goal.size()-1;
-        while(j<s.size()){
-            if(s.substr(i,goal.size())==goal.substr(0,goal.size())) return true;
-            i++;
-            j++;
+        for (int i = 0; i <= s.size() - goal.size(); i++) {
+            if (s.substr(i, goal.size()) == goal)
+                return true;
         }
         return false;
     }
