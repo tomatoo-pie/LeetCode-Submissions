93234d07051ce5e575c10afd264bdfa04646171b
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-11 01:22:53 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.96%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int ans = 0;
-        s.erase(0,s.find_first_not_of(' '));
-        if(s.empty())return 0;
-        if(s[0]=='-' || s[0]=='+'){
-            int i = 1;
-            while(i<s.size() && isdigit(s[i])){
-                if(ans > (INT_MAX - (s[i]-'0')) / 10){if(s[0]=='-')return INT_MIN;else return INT_MAX;}
-                ans = ans*10 + (s[i]-'0');
-                i++;
-            }
-            if(s[0]=='-')return -ans;
-            return ans;
+        s.erase(0, s.find_first_not_of(' '));
+
+        if (s.empty()) return 0;
+
+        int sign = 1;
+        int i = 0;
+
+        if (s[i] == '-') {
+            sign = -1;
+            i++;
         }
-        else if(isdigit(s[0])) {
-            int i = 0;
-            while(i<s.size() && isdigit(s[i])){
-                if(ans > (INT_MAX - (s[i]-'0')) / 10)return INT_MAX;
-                ans = ans*10 + (s[i]-'0');
-                i++;
-            }
-            return ans;;
+        else if (s[i] == '+') {
+            i++;
         }
-        return 0;
+
+        long long ans = 0;
+
+        while (i < s.size() && isdigit(s[i])) {
+            ans = ans * 10 + (s[i] - '0');
+
+            if (sign == 1 && ans > INT_MAX)
+                return INT_MAX;
+
+            if (sign == -1 && -ans < INT_MIN)
+                return INT_MIN;
+
+            i++;
+        }
+
+        return sign * ans;
     }
 };
