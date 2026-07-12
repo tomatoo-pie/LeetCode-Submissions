0702f88086d055049ec9f5569151a0b0bb049264
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-12 13:55:01 +0530

    Time: 11 ms (60.9%), Space: 18.5 MB (40.89%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    bool ispalindrome(string &s, int i, int j){
-        while(i<=j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
+    string longestpalindromesubstring(string &s ,int i, int j){
+        while(i>=0 && j<s.size() && s[i]==s[j]){
+            i--;
+            j++;
         }
-        return true;
+        return s.substr(i+1,j-(i+1));
     }
 
     string longestPalindrome(string s) {
+        if(s.length()==1) return s;
 
         string ans = "";
+        string maxlen = "";
 
         for(int i = 0; i < s.size(); i++) {
-            for(int j = i; j < s.size(); j++) {
-                if(ispalindrome(s,i,j) && j-i+1>ans.size()){
-                    ans = s.substr(i,j-i+1);
-                }
-            }
+            string odd = longestpalindromesubstring(s, i, i);
+            string even = longestpalindromesubstring(s, i, i + 1);
+
+            if (odd.length() > maxlen.length())
+                maxlen = odd;
+
+            if (even.length() > maxlen.length())
+                maxlen = even;
         }
 
-        return ans;
+        return maxlen;
     }
 };
