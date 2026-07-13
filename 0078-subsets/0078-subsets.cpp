abb7bd02f6656d079a0be52160e927a75f9ab85e
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-13 15:46:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (82.34%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> returnindex(string s,vector<int>& nums){
+        vector<int> v;
+        reverse(s.begin(),s.end());
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i]=='1')v.push_back(nums[i]);
+        }
+        return v;
+    }
+
+    void addOne(string &s) {
+    int i = s.size() - 1;
+
+        while (i >= 0) {
+            if (s[i] == '0') {
+                s[i] = '1';
+                return;
+            }
+
+            s[i] = '0';
+            i--;
+        }
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subset;
+        string s = string(nums.size(),'0');
+        for(int i = 0 ; i < (1<<nums.size()) ; i++){
+            subset.push_back(returnindex(s,nums));
+            addOne(s);
+        }
+        return subset;
+    }
+};
