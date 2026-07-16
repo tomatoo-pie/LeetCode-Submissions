56b187b2e3d9d9e40e29ddefd7630edb155ee1d7
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-16 19:05:30 +0530

    Time: 0 ms (100%), Space: 10.5 MB (50.8%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    void check(int index,vector<int> &nums,vector<int> &ans,set<vector<int>> &st){
-        if(index==nums.size()){
-            st.insert(ans);
-            return;
+    void check(int index,vector<int> &nums,vector<int> &ans,vector<vector<int>> &v){
+        v.push_back(ans);
+
+        for(int i=index;i<nums.size();i++){
+            if(i>index && nums[i]==nums[i-1])
+                continue;
+
+            ans.push_back(nums[i]);
+            check(i+1,nums,ans,v);
+            ans.pop_back();
         }
-
-        check(index+1,nums,ans,st);
-
-        ans.push_back(nums[index]);
-        check(index+1,nums,ans,st);
-        ans.pop_back();
-
     }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> st;
+        vector<vector<int>> v;
         vector<int> ans;
         sort(nums.begin(),nums.end());
-        check(0,nums,ans,st);
-        vector<vector<int>> v;
-        for(auto x : st){
-            v.push_back(x);
-        }
-
+        check(0,nums,ans,v);
         return v;
     }
 };
