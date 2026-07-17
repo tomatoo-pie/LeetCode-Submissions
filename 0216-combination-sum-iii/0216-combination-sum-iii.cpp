1a58c0a4b6ed7fc13238152c9ed9a23c64ae3d8a
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-17 16:20:47 +0530

    Time: 0 ms (100%), Space: 9 MB (24.66%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void check(int idx,int n,vector<int> &pair,vector<vector<int>> &ans,int k){
+        if(pair.size()==k){
+            if(n==0)ans.push_back(pair);
+            return;
+        }
+
+        if(idx>=9)return;
+
+        idx += 1;
+        pair.push_back(idx);
+        check(idx,n-idx,pair,ans,k);
+        pair.pop_back();
+
+        check(idx,n,pair,ans,k);
+
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> pair;
+        int target = n;
+        check(0,n,pair,ans,k);
+        return ans;
+    }
+};
