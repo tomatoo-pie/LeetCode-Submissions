508c6b8e125d74d7d387e9a743d86f44b1275589
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-19 02:11:30 +0530

    Time: 7 ms (40.42%), Space: 26.4 MB (16.37%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         vector<int> l;
         for(int i =0;i<nums.size();i++){
             mp[nums[i]]++;
