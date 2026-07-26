fdfc6d5b8c1901a8789b91ec0b379b872b4df26d
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-26 19:02:20 +0530

    Time: 0 ms (100%), Space: 12.9 MB (48.51%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -16,11 +16,7 @@ public:
         if(p == nullptr || q == nullptr) return false;
         if(p->val != q->val)return false;
 
-        if( !isSameTree(p->left,q->left)) 
-        return false;
-        if(!isSameTree(p->right,q->right)) 
-        return false;
-        return true;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
 
         
     }
