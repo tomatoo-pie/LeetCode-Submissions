66337d265e07567baff9340d84b225ef9d789bbe
Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-07-30 17:36:39 +0530

    Time: 0 ms (100%), Space: 19.8 MB (45.56%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -19,7 +19,7 @@ public:
 
         // {node, index}
         queue<pair<TreeNode*, long long>> q;
-        q.push({root, 0});
+        q.push({root, 1});
 
         while (!q.empty()) {
             int n = q.size();
@@ -36,11 +36,11 @@ public:
                 if (i == n - 1) last = currIndex;
 
                 if (node->left) {
-                    q.push({node->left, 2 * currIndex + 1});
+                    q.push({node->left, 2 * currIndex});
                 }
 
                 if (node->right) {
-                    q.push({node->right, 2 * currIndex + 2});
+                    q.push({node->right, 2 * currIndex + 1});
                 }
             }
 
