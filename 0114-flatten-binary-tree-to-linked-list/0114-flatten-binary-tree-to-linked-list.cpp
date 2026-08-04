d265a9da8613c3037796337e497a0425444e9e44
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-08-04 17:37:10 +0530

    Time: 0 ms (100%), Space: 17.8 MB (16.03%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,38 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<TreeNode*> &pre){
-        if(root==nullptr) return;
+    void preorder(TreeNode* root, vector<TreeNode*>& pre) {
+        if (!root) return;
 
         pre.push_back(root);
-        preorder(root->left,pre);
-        preorder(root->right,pre);
+        preorder(root->left, pre);
+        preorder(root->right, pre);
     }
 
     void flatten(TreeNode* root) {
-        if(root==nullptr) return;
+        if (!root) return;
+
         vector<TreeNode*> pre;
-        preorder(root,pre);
-        
-        root = pre[0];
-        root->left = nullptr;
-        root->right = nullptr;
-        TreeNode* temp = root;
-        for(int i = 0 ; i < pre.size()-1 ; i++){
-            temp->right = pre[i+1];
-            temp = temp->right;
-            temp->left = nullptr;
-            temp->right = nullptr;
+        preorder(root, pre);
+
+        for (int i = 0; i < pre.size() - 1; i++) {
+            pre[i]->left = nullptr;
+            pre[i]->right = pre[i + 1];
         }
+
+        pre.back()->left = nullptr;
+        pre.back()->right = nullptr;
     }
 };
