a2f31c5dade0e9fccaaae167fd759dfa65349ce7
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-08-06 19:08:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (97.62%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int digitproduct(int n){
-        int k  = n;
-        if(n%10 == 0 ) return 0;
-        int product=1;
-        int digit;
-        while(k>0){
-            digit = k%10;
-            product *= digit;
-            k = k/10;
+    int digitproduct(int n) {
+        if (n == 0) return 0;
+
+        int product = 1;
+        while (n > 0) {
+            product *= (n % 10);
+            n /= 10;
         }
         return product;
     }
 
     int smallestNumber(int n, int t) {
-        if(digitproduct(n)%t==0)return n;
-        return smallestNumber(n+1,t);
+        while (digitproduct(n) % t != 0)
+            n++;
+        return n;
     }
 };
