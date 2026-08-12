1c0d179aa44039363d324301f5f5b78cd429ccce
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-08-12 16:28:33 +0530

    Time: 0 ms (100%), Space: 10.8 MB (38.21%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int n,int id,vector<int>& dp){
-        if(id>=n){
-            return 0;
-        }
-
-        if(dp[id] != -1){
-            return dp[id];
-        }
-
-        int rob = nums[id] + solve(nums,n,id+2,dp);
-        int skip = solve(nums,n,id+1,dp);
-
-        return dp[id] = max(rob,skip);
-    }
-
     int rob(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
-        vector<int> dp1(nums.size(),-1);
-        vector<int> dp2(nums.size(),-1);
-        return max(solve(nums,nums.size()-1,0,dp1),solve(nums,nums.size(),1,dp2));
+        int n = nums.size();
+        vector<int> dp1(n + 2, 0);
+        vector<int> dp2(n + 2, 0);
+
+        for (int id = n - 1; id > 0; id--) {
+            int rob = nums[id] + dp1[id + 2];
+            int skip = dp1[id + 1];
+
+            dp1[id] = max(rob, skip);
+        }
+
+        for (int id = n - 2; id >= 0; id--) {
+            int rob = nums[id] + dp2[id + 2];
+            int skip = dp2[id + 1];
+
+            dp2[id] = max(rob, skip);
+        }
+
+        return max(dp1[1],dp2[0]);
     }
 };
