6da283b8454c0dc52127a8eaaaa68e752929321c
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-08-13 18:03:08 +0530

    Time: 331 ms (32.22%), Space: 113.5 MB (28.86%) - LeetHub

---
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.cpp
@@ -0,0 +1,96 @@
+class Solution {
+public:
+    struct Node {
+        int pref, suff, best, len;
+        char lc, rc;
+
+        Node() : pref(0), suff(0), best(0), len(0), lc(0), rc(0) {}
+        
+        Node(char c) {
+            pref = suff = best = len = 1;
+            lc = rc = c;
+        }
+    };
+
+    vector<Node> seg;
+    string s;
+
+    Node merge(Node a, Node b) {
+        if (a.len == 0) return b;
+        if (b.len == 0) return a;
+
+        Node res;
+        res.len = a.len + b.len;
+        res.lc = a.lc;
+        res.rc = b.rc;
+
+        res.pref = a.pref;
+        res.suff = b.suff;
+        res.best = max(a.best, b.best);
+
+        if (a.rc == b.lc) {
+            res.best = max(res.best, a.suff + b.pref);
+
+            if (a.pref == a.len)
+                res.pref = a.len + b.pref;
+
+            if (b.suff == b.len)
+                res.suff = b.len + a.suff;
+        }
+
+        return res;
+    }
+
+    void build(int node, int l, int r) {
+        if (l == r) {
+            seg[node] = Node(s[l]);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+
+        seg[node] = merge(seg[node * 2], seg[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r, int idx, char c) {
+        if (l == r) {
+            seg[node] = Node(c);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid)
+            update(node * 2, l, mid, idx, c);
+        else
+            update(node * 2 + 1, mid + 1, r, idx, c);
+
+        seg[node] = merge(seg[node * 2], seg[node * 2 + 1]);
+    }
+
+    vector<int> longestRepeating(string s, string queryCharacters,
+                                 vector<int>& queryIndices) {
+        this->s = s;
+
+        int n = s.size();
+        seg.resize(4 * n);
+
+        build(1, 0, n - 1);
+
+        vector<int> ans;
+
+        for (int i = 0; i < queryCharacters.size(); i++) {
+            int idx = queryIndices[i];
+            char c = queryCharacters[i];
+
+            update(1, 0, n - 1, idx, c);
+
+            ans.push_back(seg[1].best);
+        }
+
+        return ans;
+    }
+};
