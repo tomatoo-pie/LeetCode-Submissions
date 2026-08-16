d9353309edc3c34bf66d7ce855a7165a2a01b785
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-08-16 13:30:49 +0530

    Time: 0 ms (100%), Space: 171.2 MB (55.03%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int sl(vector<int>& nums,int id,int xr,int length,vector<int>& dp){
-        if(id < 0){
-            if (xr != 0) return length;
-            return 0;
-        }
-
-        if(dp[id] != -1){
-            return dp[id];
-        }
-
-        int take = sl(nums,id-1,nums[id]^xr,length+1,dp);
-        int nottake = sl(nums,id-1,xr,length,dp);
-
-        return dp[id] = max(take,nottake);
-    }
-
     int longestSubsequence(vector<int>& nums) {
-        vector<int> temp = {0,0,7,0,0,0,7,0,0};
-        if(temp == nums)
-        return 8;
+        int xr = 0;
+        bool hasNonZero = false;
 
-        vector<int> dp(nums.size()+1,-1);
-        return sl(nums,nums.size()-1,0,0,dp);
+        for (int x : nums) {
+            xr ^= x;
+            if (x != 0)
+                hasNonZero = true;
+        }
 
-        
+        if (xr != 0)
+            return nums.size();
+
+        if (hasNonZero)
+            return nums.size() - 1;
+
+        return 0;
     }
 };
