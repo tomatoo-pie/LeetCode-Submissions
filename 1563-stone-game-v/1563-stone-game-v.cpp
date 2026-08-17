55bde4f06148f28f9f5af9daf792f198e0486f63
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-08-17 14:21:56 +0530

    Time: 919 ms (16.72%), Space: 27.5 MB (73.18%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+
+        // Prefix sum
+        vector<int> prefix(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + stoneValue[i];
+        }
+
+        // dp[l][r] = maximum score from l to r
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // Length 2 -> n
+        for (int len = 2; len <= n; len++) {
+
+            for (int l = 0; l + len - 1 < n; l++) {
+
+                int r = l + len - 1;
+
+                for (int k = l; k < r; k++) {
+
+                    int leftSum = prefix[k + 1] - prefix[l];
+                    int rightSum = prefix[r + 1] - prefix[k + 1];
+
+                    if (leftSum < rightSum) {
+                        dp[l][r] = max(
+                            dp[l][r],
+                            leftSum + dp[l][k]
+                        );
+                    }
+
+                    else if (leftSum > rightSum) {
+                        dp[l][r] = max(
+                            dp[l][r],
+                            rightSum + dp[k + 1][r]
+                        );
+                    }
+
+                    else {
+                        dp[l][r] = max(
+                            dp[l][r],
+                            leftSum + max(dp[l][k], dp[k + 1][r])
+                        );
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+};
