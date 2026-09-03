3615a6cb3b04d39c80e0a9e412000c830583213b
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-09-03 18:00:58 +0530

    Time: 4 ms (65.18%), Space: 165.9 MB (31.94%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums) {
+
         int minOdd = INT_MAX;
 
         for (int x : nums) {
-            if (x % 2 != 0) {
+            if (x % 2 != 0)
                 minOdd = min(minOdd, x);
-            }
         }
 
+        // No odd numbers -> already all even
+        if (minOdd == INT_MAX)
+            return true;
+
         for (int x : nums) {
-            if (x % 2 == 0 && minOdd != INT_MAX && x < minOdd) {
+
+            // Odd numbers can stay odd
+            if (x % 2 != 0)
+                continue;
+
+            // Even x must subtract a smaller odd number
+            if (x - minOdd < 1)
                 return false;
-            }
         }
 
         return true;
