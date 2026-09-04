3350dac7583ea9941525c17b535e66354ef53944
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-09-04 21:14:52 +0530

    Time: 3 ms (25.17%), Space: 31.2 MB (56.73%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int t) {
-        int maxi = INT_MIN;
-        for(int i = 0; i<nums.size();i++){
-            maxi = max(maxi,nums[i]);
-            priority_queue<int> heapq;
-            int j = i;
-            while(j<nums.size()){
-                heapq.push(-nums[j]);
-                j++;
-            }
+        int n = nums.size();
+        vector<int> suffixMin(n);
 
-            if(maxi + heapq.top() <= t) return i;
+        suffixMin[n - 1] = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--)
+            suffixMin[i] = min(nums[i], suffixMin[i + 1]);
+
+        int maxi = INT_MIN;
+
+        for(int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+
+            if(maxi - suffixMin[i] <= t)
+                return i;
         }
+
         return -1;
     }
 };
