5321df99e038471259afd5e3ab0bd97147045196
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-09-21 01:15:47 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.66%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,8 +3,8 @@ public:
     int reverseDegree(string s) {
         int sum = 0;
         for(int i = 0; i < s.length();i++){
-            sum += (((s[i]-'a') - 26) * (i+1));
+            sum += ((26 - (s[i]-'a')) * (i+1));
         }
-        return abs(sum);
+        return sum;
     }
 };
