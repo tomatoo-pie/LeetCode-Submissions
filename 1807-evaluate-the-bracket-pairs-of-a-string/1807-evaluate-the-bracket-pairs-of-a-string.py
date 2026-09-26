a903d6c8a6d7bc228b500274a959748b27efe90c
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-09-26 17:24:21 +0530

    Time: 43 ms (82.55%), Space: 51.5 MB (61.68%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -6,12 +6,14 @@ class Solution:
 
         while i < len(s):
             if s[i] == '(':
-                j = s.index(')', i)
-                key = s[i + 1:j]
-                ans.append(mp.get(key, '?'))
-                i = j + 1
+                j = i
+                while s[j] != ')':
+                    j+=1
+                key = s[i+1:j]
+                ans.append(mp.get(key,'?'))
+                i = j+1
             else:
                 ans.append(s[i])
-                i += 1
+                i+=1
 
         return ''.join(ans)
